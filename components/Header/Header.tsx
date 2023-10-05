8d9dcf0c1b9f654807038f2a1bff86a4678eea93
SaniEHerr <santiagoeherrera@gmail.com>
2023-10-05 15:44:26 -0300

    Header and Main basic layout

---
+++ components/Header/Header.tsx
@@ -0,0 +1,74 @@
+"use client"
+
+import { twMerge } from "tailwind-merge";
+import { useRouter } from "next/navigation";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx"
+import { HiHome } from "react-icons/hi"
+import { BiSearch } from "react-icons/bi"
+import Button from "../Button/Button";
+
+interface HeaderProps {
+  children: React.ReactNode;
+  clasName?: string;
+}
+
+const Header: React.FC<HeaderProps> = ( {children, clasName} ) => {
+
+  const router = useRouter() 
+
+  const handleLogout = () => {
+    // Handle logout in the future
+  }
+
+  return (
+    <div className={twMerge(`h-fit bg-gradient-to-b from-emerald-800 p-6`, clasName)}>
+      <div className="w-full mb-4 flex items-center justify-between">
+        <div className="hidden md:flex gap-x-2 items-center">
+          <button 
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-65 transition"
+            onClick={() => router.back()}
+          >
+            <RxCaretLeft size={35} className="text-white" />
+          </button>
+          <button 
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
+            onClick={() => router.forward()}
+          >
+            <RxCaretRight size={35} className="text-white" />
+          </button>
+        </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <HiHome className="text-black" size={20} />
+          </button>
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <BiSearch className="text-black" size={20} />
+          </button>
+        </div>
+        <div className="flex justify-between items-center gap-x-4">
+          <>
+            <div>
+              <Button 
+                className="bg-transparent text-neutral-300 font-medium"
+                onClick={() => {}}
+              >
+                Sign Up
+              </Button>
+            </div>
+            <div>
+              <Button 
+                className="bg-white px-6 py-2"
+                onClick={() => {}}
+              >
+                Log in
+              </Button>
+            </div>
+          </>
+        </div>
+      </div>
+      {children}
+    </div>
+  )
+}
+
+export default Header
